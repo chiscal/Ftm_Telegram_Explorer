999afc6081a8504a05c7e9b1196864f41edcb7ac
paschal <paschalekems@gmail.com>
2023-03-19 18:50:32 +0100

    retry

--- src/Config/config.ts
+++ src/Config/config.ts
@@ -1,9 +1,9 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const BOT_TOKEN = process.env.BOT_TOKEN || ""
-const MORALIS = process.env.MORALIS || ""
+const BOT_TOKEN = process.env.BOT_TOKEN || "";
+const MORALIS = process.env.MORALIS || "";
 
 export const config = {
     token: BOT_TOKEN,
@@ -11,11 +11,11 @@ export const config = {
     mainnet: {
         rpc: "https://rpc.ankr.com/fantom/",
         chainId: 250,
-        name: "FTM"
+        name: "FTM",
     },
     testnet: {
         rpc: "https://rpc.testnet.fantom.network/",
         chainId: 0xfa2,
-        name: "FTM"
-    }
-}
+        name: "FTM",
+    },
+};
